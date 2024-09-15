9337082c07f72a15111fc75b4f3931039833888c
YulianVM <zerovsky@gmail.com>
2024-09-15 20:20:45 +0300

    Add files via upload

---
+++ DataTypesAndVariables/10. Calculate Speed/Program.cs
@@ -0,0 +1,7 @@
+﻿double distance = double.Parse(Console.ReadLine()); 
+double time = double.Parse(Console.ReadLine()); 
+double speed = distance / time;
+
+//начин 1
+Console.WriteLine($"{speed:F2}");
+
