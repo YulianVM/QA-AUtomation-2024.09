8ea1ebef06edd8041965626008ba6c27fbc907f2
YulianVM <zerovsky@gmail.com>
2024-09-15 20:24:59 +0300

    Add files via upload

---
+++ Key Concepts/ConsoleApp2/Program.cs
@@ -0,0 +1,4 @@
+﻿double USd = double.Parse(Console.ReadLine());
+double euro = USd * 0.88;
+
+Console.WriteLine($"{euro:F2}");
---
+++ Key Concepts/Deposit Calculator/Program.cs
@@ -0,0 +1,10 @@
+﻿//1. входни данни
+double depositAmount = Double.Parse(Console.ReadLine());
+int mounts = int.Parse(Console.ReadLine());
+double AnnualRate = double.Parse(Console.ReadLine()) / 100; //5.7 / 100 = 0.057
+
+//2 изчисляване сумата след депозита
+double finalSum = depositAmount +  mounts * (depositAmount * AnnualRate) / 12;
+
+//3. otpeàtwane na sumata sled depozita
+Console.WriteLine($"{finalSum:F2}");
